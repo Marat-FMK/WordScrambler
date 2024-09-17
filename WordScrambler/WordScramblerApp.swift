f10370e05c85551c3894646a8e831b80557f3939
MaratXcode <kosjachok8@yandex.ru>
2024-09-17 22:37:10 +0300

    Initial Commit

---
+++ WordScrambler/WordScramblerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScramblerApp.swift
+//  WordScrambler
+//
+//  Created by Marat Fakhrizhanov on 17.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct WordScramblerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
